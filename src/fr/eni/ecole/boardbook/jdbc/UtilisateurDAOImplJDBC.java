2d8ac2a32141b48fc612d1056c85aec48e1797c4
victorien <victorien@eni.fr>
2018-02-13 14:26:53 +0100

    SelectByIdentifiant

--- src/fr/eni/ecole/boardbook/jdbc/UtilisateurDAOImplJDBC.java
+++ src/fr/eni/ecole/boardbook/jdbc/UtilisateurDAOImplJDBC.java
@@ -2,6 +2,7 @@ package fr.eni.ecole.boardbook.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.List;
 import fr.eni.ecole.boardbook.bo.Fiche;
 import fr.eni.ecole.boardbook.bo.Utilisateur;
 import fr.eni.ecole.boardbook.dal.DAO;
+import fr.eni.ecole.boardbook.dal.DBConnection;
 
 public class UtilisateurDAOImplJDBC implements DAO<Utilisateur>{
 	
@@ -17,6 +19,8 @@ public class UtilisateurDAOImplJDBC implements DAO<Utilisateur>{
 	private PreparedStatement pstmt;
 	private Statement stmt;
 	private List<Utilisateur> listUtilisateurs = new ArrayList<>();
+	
+	private static final String SQL_SELECT_BY_ID = "SELECT * FROM UTILISATEUR WHERE identifiant=? and mdp=?";
 
 	@Override
 	public void insert(Utilisateur data) throws SQLException {
@@ -45,7 +49,34 @@ public class UtilisateurDAOImplJDBC implements DAO<Utilisateur>{
 	@Override
 	public Utilisateur selectByIdentifiant(String identifiant, String mdp) throws SQLException {
 		// TODO Auto-generated method stub
-		return null;
+		con = null;
+		pstmt = null;
+		ResultSet rs = null;
+		Utilisateur utilisateur = null;
+		try {
+			con = DBConnection.getConnection();
+			pstmt = con.prepareStatement(SQL_SELECT_BY_ID);
+			pstmt.setString(1, identifiant);
+			pstmt.setString(2, mdp);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				utilisateur = new Utilisateur(rs.getString("nom"), rs.getString("prenom"), rs.getString("identifiant"), rs.getString("mdp"), rs.getBoolean("conducteur"), rs.getBoolean("administrateur"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return utilisateur;
 	}
 
 	@Override
