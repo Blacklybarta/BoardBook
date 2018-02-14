3cb129960aa39ffddd6e8da990837c8bcc957129
Nico <nico@eni.fr>
2018-02-14 16:49:57 +0100

    coucou michou

--- src/fr/eni/ecole/boardbook/servlet/DoUserAjout.java
+++ src/fr/eni/ecole/boardbook/servlet/DoUserAjout.java
@@ -14,8 +14,7 @@ public class DoUserAjout extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		
+			
 		try {
 			req.setAttribute("listeLieux", DAOFactory.getLieuDAO().selectAll());
 			req.setAttribute("listeType", DAOFactory.getDeplacementDAO().selectAll());
