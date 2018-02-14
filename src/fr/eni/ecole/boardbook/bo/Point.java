200c887a9ae3d093644d9d2a9b9ef2950a40b211
Nico <nico@eni.fr>
2018-02-14 10:32:11 +0100

    Ajout de la classe générique Point

---
+++ src/fr/eni/ecole/boardbook/bo/Point.java
@@ -0,0 +1,37 @@
+package fr.eni.ecole.boardbook.bo;
+
+public class Point <X, Y, Z>{
+	private X x;
+	private Y y;
+	private Z z;
+	
+	public Point (){
+		
+	}
+
+	public X getX() {
+		return x;
+	}
+
+	public void setX(X x) {
+		this.x = x;
+	}
+
+	public Y getY() {
+		return y;
+	}
+
+	public void setY(Y y) {
+		this.y = y;
+	}
+
+	public Z getZ() {
+		return z;
+	}
+
+	public void setZ(Z z) {
+		this.z = z;
+	}
+	
+	
+}
